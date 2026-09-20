362686a0fbe6df31751d32575d918d854a1bd7ff
sidharthsaji04-sys <sidharthsaji04@gmail.com>
2026-09-20 21:15:48 +0530

    studied deeply about the scientific side

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Aim- Investigate how parental genotype and population allele frequency influence the simulated probability of an autosomal-recessive disorder across successive generations
+
 import random as rd
 def autosomal_recessive(parent1,parent2):
    offspring=[]
@@ -11,7 +13,7 @@ def autosomal_recessive(parent1,parent2):
    affected=0
    for off in offspring:
     partner=rd.choice(possible_genos)
-    print(f'{partner} - {off}')
+    print(f'{off} - {partner}')
     for allele1 in off:
       for allele2 in partner:
         f2=allele1 + allele2
@@ -27,8 +29,9 @@ def autosomal_recessive(parent1,parent2):
            affected+=1
            print(f2, '-affected😷')
    print("Total analysis: ")
-   print('Total normal- ',normal)
-   print('Total carrier- ',carrier)
-   print('Total affected- ',affected)
+   total = normal + carrier + affected
+   print(f'Total normal- {normal/total*100:.1f}%',)
+   print(f'Total carrier- {carrier/total*100:.1f}%')
+   print(f'Total affected- {affected/total*100:.1f}%')
 
 autosomal_recessive('AA','Aa')
