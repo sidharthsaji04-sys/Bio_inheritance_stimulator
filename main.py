c1265b7d9f74c65e27a4e67f45933e4d5b7200bc
sidharthsaji04-sys <sidharthsaji04@gmail.com>
2026-09-21 18:55:31 +0530

    added some logic

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 #Aim- Investigate how parental genotype and population allele frequency influence the simulated probability of an autosomal-recessive disorder across successive generations
 #Disease - Cystic Fibrosis
 import random as rd
-def crossing(parent1,parent2):
+def crossing(parent1,parent2,q):
+   #Hardy-Weinberg calculation
+   p=1-q
+   AA=p**2
+   Aa=2*p*q
+   aa=q**2
+
    offspring=[]
    possible_genos=['AA','Aa','aa']
    for allele1 in parent1:
        for allele2 in parent2:
-        offspring.append(allele1 + allele2)
+        genotype1 = ''.join(sorted(allele1 + allele2))
+        offspring.append(genotype1)
+
    f2_offsprings=[]
    normal2=0
    carrier2=0
    affected2=0
    print('F2 generation')
    for off in offspring:
-    partner=rd.choice(possible_genos)
+    partner=rd.choices(possible_genos,weights=[AA,Aa,aa])[0]
     print(f'{off} - {partner}')
     for allele1 in off:
       for allele2 in partner:
-        f2=allele1 + allele2
+        f2 = ''.join(sorted(allele1 + allele2))
         f2_offsprings.append(f2)
         
         if f2== 'AA':
@@ -40,14 +48,14 @@ def crossing(parent1,parent2):
    carrier3=0
    affected3=0
    for off in f2_offsprings:
-     partner=rd.choice(possible_genos)
+     partner=rd.choices(possible_genos,weights=[AA,Aa,aa])[0]
      for allele1 in off:
        for allele2 in partner:
          f3=allele1 + allele2
          if f3== 'AA':
                normal3+=1
                print(f3, '-normal')
-         elif f3=='Aa' or f2=='aA':
+         elif f3=='Aa' or f3=='aA':
                carrier3+=1
                print(f3, '-carrier')
          else:
@@ -62,4 +70,4 @@ def crossing(parent1,parent2):
    print(f'Total affected- {affected3/total3*100:.1f}%')
 
 
-crossing('AA','Aa')
+crossing('AA','Aa',0.02)
