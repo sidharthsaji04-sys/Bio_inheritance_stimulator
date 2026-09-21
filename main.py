65f55fe387276d116bbbf40697ffb649686cda77
sidharthsaji04-sys <sidharthsaji04@gmail.com>
2026-09-21 18:12:49 +0530

    added gen3

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #Aim- Investigate how parental genotype and population allele frequency influence the simulated probability of an autosomal-recessive disorder across successive generations
-
+#Disease - Cystic Fibrosis
 import random as rd
-def autosomal_recessive(parent1,parent2):
+def crossing(parent1,parent2):
    offspring=[]
    possible_genos=['AA','Aa','aa']
    for allele1 in parent1:
        for allele2 in parent2:
         offspring.append(allele1 + allele2)
    f2_offsprings=[]
-   normal=0
-   carrier=0
-   affected=0
+   normal2=0
+   carrier2=0
+   affected2=0
+   print('F2 generation')
    for off in offspring:
     partner=rd.choice(possible_genos)
     print(f'{off} - {partner}')
@@ -20,18 +21,45 @@ def autosomal_recessive(parent1,parent2):
         f2_offsprings.append(f2)
         
         if f2== 'AA':
-           normal+=1
-           print(f2, '-normal😀')
+           normal2+=1
+           print(f2, '-normal')
         elif f2=='Aa' or f2=='aA':
-          carrier+=1
-          print(f2, '-carrier😕')
+          carrier2+=1
+          print(f2, '-carrier')
         else:
-           affected+=1
-           print(f2, '-affected😷')
+           affected2+=1
+           print(f2, '-affected')
    print("Total analysis: ")
-   total = normal + carrier + affected
-   print(f'Total normal- {normal/total*100:.1f}%',)
-   print(f'Total carrier- {carrier/total*100:.1f}%')
-   print(f'Total affected- {affected/total*100:.1f}%')
+   total = normal2 + carrier2 + affected2
+   print(f'Total normal- {normal2/total*100:.1f}%',)
+   print(f'Total carrier- {carrier2/total*100:.1f}%')
+   print(f'Total affected- {affected2/total*100:.1f}%')
 
-autosomal_recessive('AA','Aa')
+   print('F3 generations')
+   normal3=0
+   carrier3=0
+   affected3=0
+   for off in f2_offsprings:
+     partner=rd.choice(possible_genos)
+     for allele1 in off:
+       for allele2 in partner:
+         f3=allele1 + allele2
+         if f3== 'AA':
+               normal3+=1
+               print(f3, '-normal')
+         elif f3=='Aa' or f2=='aA':
+               carrier3+=1
+               print(f3, '-carrier')
+         else:
+               affected3+=1
+               print(f3, '-affected')
+
+   print("Total analysis: ")
+   total3 = normal3 + carrier3 + affected3
+   print('Offsprings- ', total3)
+   print(f'Total normal- {normal3/total3*100:.1f}%',)
+   print(f'Total carrier- {carrier3/total3*100:.1f}%')
+   print(f'Total affected- {affected3/total3*100:.1f}%')
+
+
+crossing('AA','Aa')
